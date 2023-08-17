6231ab236fcde7cf7d509fa3f7b7108a456eec70
Coday-meric <contact@aymeric-mai.fr>
2023-08-17 09:45:41 +0200

    Initial commit

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aymeric on 17/08/23.
+//
+
+#include "ClapTrap.hpp"
---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,14 @@
+//
+// Created by aymeric on 17/08/23.
+//
+
+#ifndef CPP_M03_CLAPTRAP_HPP
+#define CPP_M03_CLAPTRAP_HPP
+
+
+class ClapTrap {
+
+};
+
+
+#endif //CPP_M03_CLAPTRAP_HPP
